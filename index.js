fb5bb6497c04ada0fd8c4a1ea6eaf6f7efa1e4a6
felisatti-art <antoniofelisattim@gmail.com>
2026-05-27 18:15:11 +0200

    Set up server with environment variables and DB connection

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
+
+const app = require('./app');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
